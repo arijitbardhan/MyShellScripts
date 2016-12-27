8f6b7d56aea2db3b3fe92c51465af23499a75603
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-12-27 19:44:57 +0530

    remove sudo su

--- Scripts/simple-java-jenkins.sh
+++ Scripts/simple-java-jenkins.sh
@@ -1,4 +1,3 @@
-sudo su
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
